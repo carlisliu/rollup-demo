d049c242af8e69e661ec5d39f5dca9077d6231f4
Carlis Liu <carlisliu@live.com>
2016-11-08 19:28:52 +0800

    add plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,28 @@
 import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
-function metric(ty_rum) {
-    ty_rum.server = {};
+function config(ty_rum) {
+  ty_rum.server = {};
+  //ty_rum.agent = {};
 }
 
 export default {
   entry: 'src/index.js',
   //entry: 'src/main.js',
   format: 'iife',
-  plugins: [json()],
-  dest: 'bundle.js', // equivalent to --output
-  moduleName: 'demo',
-  interop: false,
   globals: {
     window: 'window',
-    config: '{server: "192.168.1.1"}',
-    metric: metric.toString()
-  }
+    global: '{server: "192.168.1.1"}',
+    config: config.toString()
+  },
+  plugins: [json(), nodeResolve({
+    jsnext: true,
+    main: true,
+    browser: true,
+    skip: ['global', 'config', 'window']
+  }), commonjs()],
+  dest: 'dist/bundle.js', // equivalent to --output
+  moduleName: 'TINGYUN',
+  interop: false
 };
